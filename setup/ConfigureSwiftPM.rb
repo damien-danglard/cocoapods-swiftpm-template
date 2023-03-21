353d0168f354b6802203d3c824e2531c146f5c0f
Damien DANGLARD <ddanglard@cegid.com>
2023-03-21 16:54:38 +0100

    fix: try to solve touch pb

--- setup/ConfigureSwiftPM.rb
+++ setup/ConfigureSwiftPM.rb
@@ -33,6 +33,7 @@ module Pod
 
       # There has to be a single file in the Classes dir
       # or a framework won't be created
+      `pwd`
       `touch Sources/#{@configurator.pod_name}/#{@configurator.pod_name}.swift`
 
       `mv ./templates/swift-pm/* ./`
