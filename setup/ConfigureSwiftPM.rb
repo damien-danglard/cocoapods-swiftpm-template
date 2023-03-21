9227e7061a11f4d741151718a177242c38e47c2b
Damien DANGLARD <ddanglard@cegid.com>
2023-03-21 15:37:00 +0100

    feat: add swiftpm config

---
+++ setup/ConfigureSwiftPM.rb
@@ -0,0 +1,42 @@
+module Pod
+
+  class ConfigureSwiftPM
+    attr_reader :configurator
+
+    def self.perform(options)
+      new(options).perform
+    end
+
+    def initialize(options)
+      @configurator = options.fetch(:configurator)
+    end
+
+    def perform
+      framework = configurator.ask_with_answers("Which testing frameworks will you use", ["Quick", "None"]).to_sym
+      case framework
+        when :quick
+          configurator.add_pod_to_podfile "Quick', '~> 2.2.0"
+          configurator.add_pod_to_podfile "Nimble', '~> 10.0.0"
+          configurator.set_test_framework "quick", "swift", "swift"
+
+        when :none
+          configurator.set_test_framework "xctest", "swift", "swift"
+      end
+
+      Pod::ProjectManipulator.new({
+        :configurator => @configurator,
+        :xcodeproj_path => "templates/swift/Example/PROJECT.xcodeproj",
+        :platform => :ios,
+        :remove_demo_project => true,
+        :prefix => ""
+      }).run
+
+      # There has to be a single file in the Classes dir
+      # or a framework won't be created
+      `touch Pod/Classes/#{@configurator.pod_name}.swift`
+
+      `mv ./templates/swift/* ./`
+    end
+  end
+
+end
